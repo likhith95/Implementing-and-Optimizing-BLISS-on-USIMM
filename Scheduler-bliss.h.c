a7ff00ab637a1fbf21f7d8233a887441aefe6cf6
Likhith <44422005+likhith95@users.noreply.github.com>
2019-12-17 15:26:56 -0600

    Add files via upload

---
+++ Scheduler-bliss.h.c
@@ -0,0 +1,7 @@
+#ifndef __SCHEDULER_H__
+#define __SCHEDULER_H__
+void init_scheduler_vars(); 14 //called from main
+void scheduler_stats(); //called from main 
+void schedule(int); 10 // scheduler function called every cycle
+
+#endif //__SCHEDULER_H__
